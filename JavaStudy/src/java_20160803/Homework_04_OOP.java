0af1a8ac1d74db08ac8da9432aad7e85db2caf0f
choiwj1012 <choiwj1012@hanmail.net>
2016-08-03 18:13:11 +0900

    today fixed

--- JavaStudy/src/java_20160803/Homework_04_OOP.java
+++ JavaStudy/src/java_20160803/Homework_04_OOP.java
@@ -21,9 +21,9 @@ public class Homework_04_OOP {
 	static String[] articleTitles = new String[MAX_LIST_COUNT];   //글제목		
 	static String[] articleWriters = new String[MAX_LIST_COUNT];  //작성자
 	static String[] articleContents = new String[MAX_LIST_COUNT]; //글내용
-	static int[] articleNumber = new int[MAX_LIST_COUNT]; 		   //글번호
-	static int[] articleCount = new int[MAX_LIST_COUNT]; 		   //조회수
-	static int currentPosition = 0; 	// 글의 총 갯수		
+	static int[] articleNumber = new int[MAX_LIST_COUNT]; 		  //글번호
+	static int[] articleReadCount = new int[MAX_LIST_COUNT]; 	  //조회수
+	static int totalArticleNumber = 0; 	// 글의 총 갯수		
 	
 	public static void main(String[] args) {
 	
@@ -91,12 +91,13 @@ public class Homework_04_OOP {
 			System.out.print("작성자를 입력해 주세요 >");
 			String writer = keyboard.nextLine();
 			
-			articleNumber[currentPosition] = currentPosition + 1; // 글번호 1씩 증가
-			articleTitles[currentPosition] = title; // 제목 입력
-			articleContents[currentPosition] = content; // 내용 입력
-			articleWriters[currentPosition] = writer; // 작성자 입력
+			articleNumber[totalArticleNumber] = totalArticleNumber + 1; // 글번호 1씩 증가
+			articleTitles[totalArticleNumber] = title; // 제목 입력
+			articleContents[totalArticleNumber] = content; // 내용 입력
+			articleWriters[totalArticleNumber] = writer; // 작성자 입력
+			articleReadCount[totalArticleNumber] = 0;
 			
-			currentPosition = currentPosition + 1; // 글갯수 1씩 증가
+			totalArticleNumber = totalArticleNumber + 1; // 글갯수 1씩 증가
 			
 			System.out.println("작성이 완료되었습니다. 한번 더 쓰시겠습니까? yes or no");
 			String checkList = keyboard.next();
@@ -125,10 +126,12 @@ public class Homework_04_OOP {
 			
 			System.out.println("보시고 싶은 글번호를 입력하세요");
 			boolean isFind = false; // 없는 글번호 선택시 다음 행동을 판단하는 기준
-			int selectedMenu = keyboard.nextInt();
+			int selectedArtNum = keyboard.nextInt();
 			
-			for(int i=0; i<currentPosition; i++){
-				if(selectedMenu == articleNumber[i]){ // 선택한 글번호를 찾으면
+			for(int i=0; i<totalArticleNumber; i++){
+				if(selectedArtNum == articleNumber[i]){ // 선택한 글번호를 찾으면
+					//articleReadCount[selectedArtNum] = articleReadCount[selectedArtNum] + 1; 작동 안함
+					articleReadCount[i] = articleReadCount[i] + 1; // 조회수 1 증가
 					printList(i);					  // 리스트를 출력	
 					isFind = true;					  // 글번호를 찾았으므로 true로 변경
 				}
@@ -139,28 +142,32 @@ public class Homework_04_OOP {
 				readArticle();						  // 다시 입력창으로 돌아감	
 			}
 			
-			isFind = false;						      // 변수 초기화
-			
-			System.out.println("다시 보시겠습니까? yes or no");
-			String checkList = keyboard.next();
-			
-			if(checkList.equals("yes") || checkList.equals("YES")){	// 다시보겠다고 하면 재실행
-				
-				continue;
-				
-			}else if(checkList.equals("no")||checkList.equals("NO")){ // 다시 안보겠다고 하면 메인메뉴로 복귀
-				
-				break;
-				
-			}else{
-				
-				System.out.println("잘못 쓰셨습니다. yes나 no로 입력해주세요");
-				
-			}
+			review();
 			
 		} // while end
 		
 	} // readArticle() end 
+	
+	// 다시보기
+	public static void review(){
+		
+		System.out.println("다시 보시겠습니까? yes or no");
+		String checkList = keyboard.next();
+		
+		if(checkList.equals("yes") || checkList.equals("YES")){	// 다시보겠다고 하면 재실행
+			
+			readArticle();
+			
+		}else if(checkList.equals("no")||checkList.equals("NO")){ // 다시 안보겠다고 하면 메인메뉴로 복귀
+			
+			mainMenu();
+			
+		}else{
+			
+			System.out.println("잘못 쓰셨습니다. yes나 no로 입력해주세요");
+			review();
+		}
+	}
 	
 	// 목록 표시 타이틀 표시
 	public static void printListTitle(){
@@ -186,7 +193,7 @@ public class Homework_04_OOP {
 		System.out.print(articleTitles[selectedArticle] + "\t");
 		System.out.print(articleContents[selectedArticle] + "\t");
 		System.out.print(articleWriters[selectedArticle] + "\t");
-		System.out.print(articleCount[selectedArticle]);
+		System.out.print(articleReadCount[selectedArticle]);
 		
 		System.out.println();
 		
@@ -199,74 +206,84 @@ public class Homework_04_OOP {
 			
 			System.out.println("수정하고 싶은 글번호를 입력하세요");
 			boolean isFind = false; // 없는 글번호 선택시 다음 행동을 판단하는 기준
-			int selectedMenu = keyboard.nextInt();
+			int selectedArticle = keyboard.nextInt();
 			
-			for(int i=0; i<currentPosition; i++){
-				if(selectedMenu == articleNumber[i]){ // 선택한 글번호를 찾으면
+			for(int i=0; i<totalArticleNumber; i++){
+				if(selectedArticle == articleNumber[i]){ // 선택한 글번호를 찾으면
+					
+					isFind = true;	// 글번호를 찾았으므로 true로 변경
+					
 					System.out.println("어떤것을 수정하시겠습니까?");
 					System.out.println("1. 제목 || 2. 작성자 || 3. 글내용 || 4. 수정완료 및 취소");
-					int selectedSwitchMenu = keyboard.nextInt();
+					int selectedModifyMenu = keyboard.nextInt();
 					
-					switch(selectedSwitchMenu){
-						case 1 :
-							// 제목 입력
-							System.out.print("제목을 입력해 주세요 >");
-							keyboard.nextLine();
-							String modifyTitle = keyboard.nextLine();
-							articleTitles[selectedMenu] = modifyTitle;
-							
-						case 2 :
-							// 작성자 입력
-							System.out.print("작성자를 입력해 주세요 >");
-							String modifyWriter = keyboard.nextLine();
-							articleWriters[selectedMenu] = modifyWriter;
-							
-						case 3 :
-							// 내용 입력
-							System.out.print("내용을 입력해 주세요 >");
-							String modifyContent = keyboard.nextLine();
-							articleContents[selectedMenu] = modifyContent;	
-							
-						case 4 :	
-							System.out.println("수정을 종료하겠습니다");
-							break;
-						default :
-							
-					} // switch end
-					
-					isFind = true;					  // 글번호를 찾았으므로 true로 변경
-					
-				} // if end
+					if(selectedModifyMenu == 1){
+						modifyTitle(selectedArticle);
+					}else if(selectedModifyMenu == 2){
+						modifyWriter(selectedArticle);
+					}else if(selectedModifyMenu == 3){
+						modifyContent(selectedArticle);
+					}else if(selectedModifyMenu == 4){
+						mainMenu();
+					}else{
+						System.out.println("잘못입력하셨습니다");
+					}
+								
+				} // if end	
 				
 			} // for end
 			
 			if(!isFind){							  // 글번호를 찾지 못했다면
 				System.out.println("없는 글번호 입니다");
-				modifyArticle();					  // 다시 입력창으로 돌아감	
+				break;					  			  // 다시 입력창으로 돌아감	
 			}
 			
-			isFind = false;						      // 변수 초기화
-			
-			System.out.println("다시 수정하시겠습니까? yes or no");
-			String checkList = keyboard.next();
-			
-			if(checkList.equals("yes") || checkList.equals("YES")){	// 다시보겠다고 하면 재실행
-				
-				continue;
-				
-			}else if(checkList.equals("no")||checkList.equals("NO")){ // 다시 안보겠다고 하면 메인메뉴로 복귀
-				
-				break;
-				
-			}else{
-				
-				System.out.println("잘못 쓰셨습니다. yes나 no로 입력해주세요");
-				
-			}
+			remodify();
 			
 		} // while end
 			
 	} // modifyArticle() end
+	
+	public static void remodify(){
+		
+		System.out.println("다시 수정하시겠습니까? yes or no");
+		String checkList = keyboard.next();
+		
+		if(checkList.equals("yes") || checkList.equals("YES")){	// 다시보겠다고 하면 재실행
+			
+			modifyArticle();
+			
+		}else if(checkList.equals("no")||checkList.equals("NO")){ // 다시 안보겠다고 하면 메인메뉴로 복귀
+			
+			mainMenu();
+			
+		}else{
+			
+			System.out.println("잘못 쓰셨습니다. yes나 no로 입력해주세요");
+			
+		}
+	}
+	
+	
+	
+	public static void modifyTitle(int selectedArticle){
+		// 제목 입력
+		System.out.print("제목을 입력해 주세요 >");
+		String modifyTitle = keyboard.next();
+		articleTitles[selectedArticle] = modifyTitle;
+	}
+	
+	public static void modifyWriter(int selectedArticle){
+		System.out.print("작성자를 입력해 주세요 >");
+		String modifyWriter = keyboard.next();
+		articleWriters[selectedArticle] = modifyWriter;
+	}
+	
+	public static void modifyContent(int selectedArticle){
+		System.out.print("내용을 입력해 주세요 >");
+		String modifyContent = keyboard.next();
+		articleContents[selectedArticle] = modifyContent;	
+	}
 	
 	// 목록보기
 	public static void articleListView(){
@@ -274,19 +291,18 @@ public class Homework_04_OOP {
 		// 목록 표시 타이틀 먼저 표시
 		printListTitle();
 		
-		for(int i=0; i<currentPosition; i++){
+		// 전체 글 목록 표시
+		for(int i=0; i<totalArticleNumber; i++){
 			System.out.print(articleNumber[i] + "\t");
 			System.out.print(articleTitles[i] + "\t");
 			System.out.print(articleContents[i] + "\t");
 			System.out.print(articleWriters[i] + "\t");
-			System.out.print(articleCount[i]);
+			System.out.print(articleReadCount[i]);
 			System.out.println();
 		}
 		
 		mainMenu();
 		
 	} // articleListView() end
-	
-	// 종료
 	
 }
