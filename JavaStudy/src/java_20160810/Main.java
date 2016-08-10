48ffe2677a7f0c6e6cfb35355cf236d2adc6b824
choiwj1012 <choiwj1012@hanmail.net>
2016-08-10 09:45:07 +0900

    commit some folder

--- JavaStudy/src/java_20160810/Main.java
+++ JavaStudy/src/java_20160810/Main.java
@@ -2,4 +2,16 @@ package java_20160810;
 
 public class Main {
 
+	public static void main(String[] args){
+		
+		// local variable
+		// - method실행시 생성되었다가 method 종료시 없어짐
+		// - memory는 stack
+		
+		int i; // declare local variable 
+		// System.out.println(i);
+		
+		
+	}
+	
 }
